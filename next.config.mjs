9c978bebf1a3d2277d62a1d25b5d6801be779ebb
Youssef-Ahmadoun <joseph.programmerr@gmail.com>
2025-06-02 14:25:19 +0100

    Build static export for GitHub Pages4

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ export default withSentryConfig(nextConfig, {
     // Suppresses source map uploading logs during build
     silent: true,
     org: "javascript-mastery",
-    project: "copywriter-portfolio",
+    project: "javascript-nextjs",
 }, {
     // For all available options, see:
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
